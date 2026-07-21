b08498a9658608157e19db812395b040c519a2fc
Yves Cabon <yves.cabon@ypc-world.com>
2026-07-21 16:13:03 +0200

    Ajout de TD3/Q3

--- TD3/Q3.py
+++ TD3/Q3.py
@@ -1,5 +1,18 @@
 from typing import Tuple
 
+def nombre(fonction):
+    def wrapper(nb):
+        if  isinstance(nb,str) and isinstance(int(nb),int):
+            try:
+                nb = int(nb)
+            except ValueError:
+                nb = 0
+        else:
+            nb = 0
+        return fonction(nb)
+    return wrapper
+
+@nombre
 def somme_chiffres(n:int)->int:
     n = abs(n)
     somme = 0
@@ -8,20 +21,23 @@ def somme_chiffres(n:int)->int:
         n //=10
     return somme
 
+@nombre
 def nombre_chiffres(n:int)->int:
     return len(str(n))
 
+@nombre
 def separe_nombre(n:int)->Tuple[int,int]:
     str_nb = str(n)
     sep = (len(str_nb)+1)//2
     return int(str_nb[:sep]),int(str_nb[sep:])
 
-def est_couicable(n:int)->bool:
+@nombre
+def est_couicable(n)->bool:
     if nombre_chiffres(n)%2 != 0:
         return False
     nb1,nb2 = separe_nombre(n)
     return somme_chiffres(nb1) == somme_chiffres(nb2)
-
+@nombre
 def somme_chiffres_rec(n:int)->int:
     n=abs(n)
     if n<10:
@@ -29,9 +45,8 @@ def somme_chiffres_rec(n:int)->int:
     return n%10 + somme_chiffres_rec(n//10)
 
 
-"""
-def nombre(fonction):
-"""
+
+
 
 
 def main():
@@ -40,7 +55,7 @@ def main():
     print("Sépare en 2 :" ,separe_nombre(12345678))
     print("Somme des chiffres :" ,somme_chiffres_rec(1245678))
 
-    n = int(input("Entrez un nombre : "))
+    n = input("Entrez un nombre : ")
     if est_couicable(n):
         print(f"{n} est couicable !")
     else:
