2950a3994a2dcf4e5124015ec75640294c4711ea
Yves Cabon <yves.cabon@ypc-world.com>
2026-07-21 14:02:37 +0200

    Ajout de TD3/Q1

--- TD3/Q1.py
+++ TD3/Q1.py
@@ -1,22 +1,18 @@
 import fcts_carre_magique as cm
 
-
-def test_carres(*args) -> None:
+def sont_carres(*args) -> None:
     for carre in args:
         print(carre)
-        print(f"Carré magique normal : {cm.est_carre_magique_normal(carre)}")
+        print(f"Carré magique  : {cm.est_carre_magique(carre)}")
         print(f"Carré magique normal : {cm.est_carre_magique_normal(carre)}")
 
 
 def main():
     mat1 = [[21, 7, 17], [11, 15, 19], [13, 23, 9]]
     mat2 = [[8, 1, 6], [3, 5, 7], [4, 9, 2]]
-    mat3 = [[21, 7, 17], [11, 15, 19], [13, 23, 9]]
-    test_carres((mat1, mat2, mat3))
+
+    sont_carres(mat1, mat2)
 
 
 if __name__ == "__main__":
     main()
-
-
-main()
