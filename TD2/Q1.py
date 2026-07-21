832d332870f7786361e604d502e86f5abc3a2a39
Yves Cabon <yves.cabon@ypc-world.com>
2026-07-21 20:41:46 +0200

    Ajout de TD4/Q1

--- TD2/Q1.py
+++ TD2/Q1.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 def somme_chiffres(n:int)->int:
     n = abs(n)
     somme = 0
@@ -9,12 +11,12 @@ def somme_chiffres(n:int)->int:
 def nombre_chiffres(n:int)->int:
     return len(str(n))
 
-def separe_nombre(n:int)->tuple:
+def separe_nombre(n:int)->Tuple[int,int]:
     str_nb = str(n)
     sep = (len(str_nb)+1)//2
     return int(str_nb[:sep]),int(str_nb[sep:])
 
-def est_couicable(n:int)->int:
+def est_couicable(n:int)->bool:
     if nombre_chiffres(n)%2 != 0:
         return False
     nb1,nb2 = separe_nombre(n)
@@ -26,13 +28,19 @@ def somme_chiffres_rec(n:int)->int:
         return n
     return n%10 + somme_chiffres_rec(n//10)
 
-print("124\nSomme des chiffres :" ,somme_chiffres(1245678))
-print("124\nNombre de chiffres :" ,nombre_chiffres(1245678))
-print("124567\nSépare en 2 :" ,separe_nombre(12345678))
-print("124\nSomme des chiffres :" ,somme_chiffres_rec(1245678))
+def main():
+    print("Somme des chiffres :" ,somme_chiffres(1245678))
+    print("Nombre de chiffres :" ,nombre_chiffres(1245678))
+    print("Sépare en 2 :" ,separe_nombre(12345678))
+    print("Somme des chiffres :" ,somme_chiffres_rec(1245678))
 
-n = int(input("Entrez un nombre : "))
-if est_couicable(n):
-    print(f"{n} est couicable !")
-else:
-    print(f"{n} n'est pas couicable.")
+    n = int(input("Entrez un nombre : "))
+    if est_couicable(n):
+        print(f"{n} est couicable !")
+    else:
+        print(f"{n} n'est pas couicable.")
+
+
+
+if __name__== "__main__":
+    main()
--- TD2/Q2.py
+++ TD2/Q2.py
@@ -17,7 +17,7 @@ def somme_diag_2(mat: List[List[int]]) -> int:
     return sum([mat[len(mat) - 1 - i][i] for i in range(0, len(mat))])
 
 
-def carre_magique(mat: List[List[int]]) -> bool:
+def est_carre_magique(mat: List[List[int]]) -> bool:
     dim = len(mat)
     somme = somme_ligne(mat, 0)
     for i in range(dim):
@@ -32,17 +32,16 @@ def carre_magique(mat: List[List[int]]) -> bool:
     return True
 
 
-def carre_magique_normal(mat: List[List[int]]) -> bool:
-    liste_chiffres = [i for i in range(1, 1+ len(mat) ** 2)]
-    print(liste_chiffres)
+def est_carre_magique_normal(mat: List[List[int]]) -> bool:
+    set_chiffres_attendus = set(i for i in range(1, 1+ len(mat) ** 2))
+    set_chiffres_trouves = set()
+
     for ligne in mat:
         for chiffre in ligne:
-            if chiffre not in liste_chiffres:
+            if chiffre not in set_chiffres_attendus:
                 return False
-            liste_chiffres.remove(chiffre)
-    if liste_chiffres:
-        return False
-    return carre_magique(mat)
+            (set_chiffres_trouves.add(chiffre))
+    return set_chiffres_trouves == set_chiffres_attendus and est_carre_magique(mat)
 
 
 def main():
@@ -51,13 +50,13 @@ def main():
     print(f"somme colonne 1 : {somme_colonne(mat1, 1)}")
     print(f"somme diag 1 : {somme_diag_1(mat1)}")
     print(f"somme diag 2 : {somme_diag_2(mat1)}")
-    print(f"magique : {carre_magique(mat1)}")
+    print(f"magique : {est_carre_magique(mat1)}")
 
     print(
-        f"Carré magique normal : {carre_magique_normal([[8, 1, 6], [3, 5, 7], [4, 9, 2]])}"
+        f"Carré magique normal : {est_carre_magique_normal([[8, 1, 6], [3, 5, 7], [4, 9, 2]])}"
     )
     print(
-        f"Carré magique normal : {carre_magique_normal([[21, 7, 17], [11, 15, 19], [13, 23, 9]])}"
+        f"Carré magique normal : {est_carre_magique_normal([[21, 7, 17], [11, 15, 19], [13, 23, 9]])}"
     )
 
 
