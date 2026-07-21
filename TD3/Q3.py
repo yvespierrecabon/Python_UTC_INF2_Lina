79a78a13ecf25f2d3ea29b99d5d1a99f5b3955ff
Yves Cabon <yves.cabon@ypc-world.com>
2026-07-21 16:50:53 +0200

    Ajout de TD3/Q3

--- TD3/Q3.py
+++ TD3/Q3.py
@@ -1,18 +1,19 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 def nombre(fonction):
-    def wrapper(*args):
-        if args:
-            nb = args[0]
-            if  isinstance(nb,str):
-                try:
-                    nb = max(0,int(nb))
-                except ValueError:
-                    nb = 0
-            elif not isinstance(nb,int|float):
-                nb = 0
-            args = (nb,) + args[1:]
-        return fonction(*args)
+    def wrapper(nb):
+        if isinstance(nb, str):
+            try:
+                nb = int(nb)
+            except ValueError:
+                raise ValueError(f"Le paramètre '{nb}' n'est pas un nombre entier valide.")
+            if nb <= 0:
+                raise ValueError(f"Le paramètre {nb} doit être un entier positif.")
+        elif not isinstance(nb, Union[int, float]):
+            raise TypeError(f"Le paramètre {nb} doit être un nombre ou une chaîne représentant un nombre.")
+        elif nb <= 0:
+            raise ValueError(f"Le paramètre {nb} doit être un entier positif.")
+        return fonction(nb)
     return wrapper
 
 @nombre
@@ -34,7 +35,7 @@ def separe_nombre(n:int)->Tuple[int,int]:
     sep = (len(str_nb)+1)//2
     return int(str_nb[:sep]),int(str_nb[sep:])
 
-
+@nombre
 def est_couicable(n)->bool:
     if nombre_chiffres(n)%2 != 0:
         return False
@@ -57,8 +58,8 @@ def main():
     print("Nombre de chiffres :" ,nombre_chiffres(1245678))
     print("Sépare en 2 :" ,separe_nombre(12345678))
     print("Somme des chiffres récursive :" ,somme_chiffres_rec(1245678))
-    print("Somme des chiffres nb négatif:" ,somme_chiffres(-1245678))
-    print("Somme des chiffres pas nombre :" ,somme_chiffres("12 45678"))
+    # print("Somme des chiffres nb négatif:" ,somme_chiffres(-1245678))
+    # print("Somme des chiffres pas nombre :" ,somme_chiffres("12 45678"))
 
     n = input("Entrez un nombre : ")
     if est_couicable(n):
