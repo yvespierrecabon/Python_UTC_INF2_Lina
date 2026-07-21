07d5364d4a24537ba3349c5b7e5ac54849732b93
Yves Cabon <yves.cabon@ypc-world.com>
2026-07-21 14:19:32 +0200

    Ajout de TD3/Q2

--- TD3/Q2.py
+++ TD3/Q2.py
@@ -13,9 +13,26 @@ def calculer_polynome(x, **kwargs)->float:
             poly += value
     return poly
 
+def calculer_polynome_2(*args, **kwargs)->float:
+    res = []
+    for x in args:
+        poly = 0
+        for key, value in kwargs.items():
+            if key == 'a':
+                poly += value*(x**3)
+            elif key == 'b':
+                poly += value*(x**2)
+            elif key == 'c':
+                poly += value*x
+            elif key == 'd':
+                poly += value
+        res.append(poly)
+    return res
+
 
 def main():
     print(calculer_polynome(3, a=1, b=2, c=3, d=4))
+    print(calculer_polynome_2(0,1,2,3,4,5, a=1, b=2, c=3, d=4))
 
 
 
