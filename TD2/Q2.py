1f0c3e7f71a2224e09e5dbbce9f19122a674ba82
Yves Cabon <yves.cabon@ypc-world.com>
2026-07-21 10:22:53 +0200

    Ajout de TD2/Q2 1 -> 6

--- TD2/Q2.py
+++ TD2/Q2.py
@@ -1,41 +1,65 @@
-from typing import *
+from typing import List
 
-def somme_ligne(mat:list, i:int)->int:
-    return sum(mat[i][:])
 
-def somme_colonne(mat:list, i:int)->int:
-    return sum(mat[:][i])
+def somme_ligne(mat: List[List[int]], i: int) -> int:
+    return sum(mat[i])
 
-def somme_diag_1(mat:list)->int:
-    return sum([mat[i][i] for i in range(0,len(mat))])
 
-def somme_diag_2(mat:list)->int:
-    return sum([mat[len(mat) -1 -i][i] for i in range(0,len(mat))])
+def somme_colonne(mat: List[List[int]], j: int) -> int:
+    return sum(row[j] for row in mat)
 
-def magique(mat:list)->bool:
+
+def somme_diag_1(mat: List[List[int]]) -> int:
+    return sum([mat[i][i] for i in range(0, len(mat))])
+
+
+def somme_diag_2(mat: List[List[int]]) -> int:
+    return sum([mat[len(mat) - 1 - i][i] for i in range(0, len(mat))])
+
+
+def carre_magique(mat: List[List[int]]) -> bool:
     dim = len(mat)
-    somme = somme_ligne(mat,0)
-
-
+    somme = somme_ligne(mat, 0)
     for i in range(dim):
-        if somme_ligne(mat,i) !=somme:
+        if somme_ligne(mat, i) != somme:
             return False
-        if  somme_colonne(mat,i) !=somme:
+        if somme_colonne(mat, i) != somme:
             return False
-    if somme_diag_1(mat) !=somme:
+    if somme_diag_1(mat) != somme:
         return False
-    if somme_diag_2(mat) !=somme:
+    if somme_diag_2(mat) != somme:
         return False
     return True
 
 
+def carre_magique_normal(mat: List[List[int]]) -> bool:
+    liste_chiffres = [i for i in range(1, 1+ len(mat) ** 2)]
+    print(liste_chiffres)
+    for ligne in mat:
+        for chiffre in ligne:
+            if chiffre not in liste_chiffres:
+                return False
+            liste_chiffres.remove(chiffre)
+    if liste_chiffres:
+        return False
+    return carre_magique(mat)
+
+
 def main():
-    mat1 = [[21,7,17],[11,15,19],[13,23,9]]
-    print(f"somme ligne 1 : {somme_ligne(mat1,1)}")
-    print(f"somme colonne 1 : {somme_colonne(mat1,1)}")
+    mat1 = [[21, 7, 17], [11, 15, 19], [13, 23, 9]]
+    print(f"somme ligne 1 : {somme_ligne(mat1, 1)}")
+    print(f"somme colonne 1 : {somme_colonne(mat1, 1)}")
     print(f"somme diag 1 : {somme_diag_1(mat1)}")
     print(f"somme diag 2 : {somme_diag_2(mat1)}")
-    print(f"magique : {magique(mat1)}")
+    print(f"magique : {carre_magique(mat1)}")
 
-if __name__== "__main__":
+    print(
+        f"Carré magique normal : {carre_magique_normal([[8, 1, 6], [3, 5, 7], [4, 9, 2]])}"
+    )
+    print(
+        f"Carré magique normal : {carre_magique_normal([[21, 7, 17], [11, 15, 19], [13, 23, 9]])}"
+    )
+
+
+if __name__ == "__main__":
     main()
