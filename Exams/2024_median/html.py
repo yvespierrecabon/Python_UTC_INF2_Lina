42ed85e310c16d27315925481a494713dcf693f5
Yves Cabon <yves.cabon@ypc-world.com>
2026-07-30 18:53:08 +0200

    Ajout de Exam 2024 html

--- Exams/2024_median/html.py
+++ Exams/2024_median/html.py
@@ -1,26 +1,32 @@
 class Balise:
-    def __init__(self, nom:str, **kwargs):
-        self.nom = nom.lower()
-        for k, v in kwargs.items():
-            setattr(self, k.lower(), v.lower())
+    def __init__(self, nom: str, attributs: dict | None = None):
+        self._nom: str = nom.lower()
+        if attributs:
+            self._attributs: dict = attributs
+
+    @property
+    def nom(self) -> str:
+        return self._nom
+
+    @property
+    def attributs(self) -> dict:
+        return self._attributs
+
+    @attributs.setter
+    def attributs(self, key, str, val: str):
+        if key in self.attributs:
+            self._attributs[key] = val
 
 
 class BaliseContenu(Balise):
-    def __init__(self, nom:str, contenu:str, **kwargs):
+    def __init__(self, nom: str, contenu: str, **kwargs):
         Balise.__init__(self, nom, **kwargs)
         self.contenu = contenu.lower()
-        
-
-
-
-
 
 
 def main():
     pass
 
 
-
 if __name__ == '__main__':
     main()
-
