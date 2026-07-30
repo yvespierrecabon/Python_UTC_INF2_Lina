91ac08eaa9073cec39f0906030fa6c654f8bf50c
Yves Cabon <yves.cabon@ypc-world.com>
2026-07-30 14:03:45 +0200

    Ajout de Exam 2023

--- Exams/2023_median/partie_2.py
+++ Exams/2023_median/partie_2.py
@@ -1,36 +1,52 @@
 class Region:
+
+    horizon:int = 0
+
     def __init__(self,titre:str) -> None:
-        self._titre = titre
-        self._position = 0
+        self._titre:str = titre
+        self._position:int = 0
 
-        @property
-        def titre(self) -> str:
-            return self._titre
+    @property
+    def titre(self) -> str:
+        return self._titre
 
-        @titre.setter
-        def titre(self,val:str) -> None:
-            self._titre = val
+    @titre.setter
+    def titre(self,val:str) -> None:
+        self._titre = val
 
-        @property
-        def position(self):
-            return self._position
+    @property
+    def position(self):
+        return self._position
 
-        @position.setter
-        def position(self,val:int) -> None:
-            if val < 0:
-                raise ValueError("Position doit être positif ou nul")
-            self._position = val
+    @position.setter
+    def position(self,val:int) -> None:
+        if val < 0:
+            raise ValueError("Position doit être positif ou nul")
+        self._position = val
+        if self._position > Region.get_horizon():
+            Region.set_horizon(self._position)
 
-        def __str__(self) -> str:
-            return f"{self.titre}"
+    def __str__(self) -> str:
+        return f"{self.titre}"
+
+    @classmethod
+    def get_horizon(cls):
+        return cls.horizon
+    @classmethod
+    def set_horizon(cls,val:int) -> None:
+        if val > cls.horizon:
+            cls.horizon = val
+
+
+
 
 class Fichier:
     def __init__(self,titre:str, duree:int) -> None:
         self._titre = titre
         self._duree = duree
 
-        def get_duree(self) -> int:
-            return self._duree
+    def get_duree(self) -> int:
+        return self._duree
 
 
 
@@ -45,15 +61,24 @@ class RegionAudio(Region):
 
     def set_portion(self, debut:int,fin:int):
         if debut >= fin:
-            raise ValueError("debut doit être superieur à fin")
+            raise ValueError("debut doit être inférieur à fin")
         self._debut = debut
         self._fin = fin
+        if self.get_position_fin() > Region.get_horizon():
+            Region.set_horizon(self.get_position_fin())
+
+
 
     def get_duree(self) -> int:
         return self._fin - self._debut
 
     def get_position_fin(self):
-        return position + self.get_duree()
+        return self._position + self.get_duree()
+
+    def __str__(self) -> str:
+        return (f"{self.titre} durée : {self.get_duree}")
+
+
 
 
 
